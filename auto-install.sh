c563f259a982bf722d939361d15ef4b7dd60b42d
Freek Bes <freekbladnl@gmail.com>
2021-11-20 06:13:21 +0100

    added update checker

--- auto-install.sh
+++ auto-install.sh
@@ -1,24 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 bold=$(tput bold)
 normal=$(tput sgr0)
 
 echo "${bold}Codam Automatic Extension Installer for Google Chrome, version 1${normal}"
 echo "================================================================"
 echo
+echo -n "Checking for updates..."
+#https://api.github.com/repos/FreekBes/codam_auto_extension_installer/contents/auto-install.sh
+onlineSha=$(curl -s https://api.github.com/repos/FreekBes/codam_auto_extension_installer/contents/auto-install.sh | grep "sha" | awk '{ print substr( $0, 11, 40) }')
+thisSha=$(shasum auto-install.sh)
+if [[ "$onlineSha" != "$thisSha" ]]; then
+	echo -e "\r\033[K${bold}An update for this script is available${normal}! Please pull it from the GitHub repository.";
+	echo "https://github.com/FreekBes/codam_auto_extension_installer"
+else
+	echo "No updates available."
+fi
+echo
 if [ "$(uname)" != "Darwin" ]; then
 	echo "This install script only works on macOS. For installing this extension on Windows or Linux, please refer to the Chrome Web Store."
 	exit
 fi
 cd ~/Library/ApplicationSupport/Google/Chrome/
-echo "Checking if External Extensions folder exists in your profile..."
 if [ ! -d "External Extensions/" ]; then
 	echo "Creating Chrome External Extensions folder in your profile..."
 	mkdir -p "External Extensions"
 fi
-echo "Now commencing with the extension installation..."
+echo "Now commencing with the extension installation."
 cd "External Extensions"
-echo
-echo
 echo "Answer ${bold}Y${normal} to install, ${bold}N${normal} to skip, for every extension listed."
 echo
 
